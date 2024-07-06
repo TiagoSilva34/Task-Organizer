3370585e17e56cd7594037abf6fa8d333c03c5e6
Tiago Borges <tiagoborges899@gmail.com>
2024-07-06 17:40:52 -0300

    fix: modificado tipo de propriedade value

--- src/shared/components/Input/interface.ts
+++ src/shared/components/Input/interface.ts
@@ -3,6 +3,6 @@ import { InputHTMLAttributes } from "react"
 export interface IInputProps extends InputHTMLAttributes<HTMLInputElement>{
     type: string 
     placeholder?: string 
-    value?: string
+    value?: any
     onChange: React.ChangeEventHandler<HTMLInputElement>
 }
