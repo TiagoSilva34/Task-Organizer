1f73d83cd7442e3f1623506c2d61cb57e53d31a6
Tiago Borges <tiagoborges899@gmail.com>
2024-07-06 17:43:30 -0300

    fix: remoção dos options e includão da props children no component Select

--- src/shared/components/Select/index.tsx
+++ src/shared/components/Select/index.tsx
@@ -1,12 +1,9 @@
 import { ISelectProps } from "./interface";
 
-export const Select: React.FC<ISelectProps> = ({ onChange, ...props }) => {
+export const Select: React.FC<ISelectProps> = ({ onChange, value, children,  ...props }) => {
   return (
-    <select onChange={(event) => onChange(event)} {...props}>
-      <option>Select a priority</option>
-      <option value="">low</option>
-      <option value="">medium</option>
-      <option value="">high</option>
+    <select value={value} onChange={(event) => onChange(event)} {...props}>
+        {children}
     </select>
   )
 
