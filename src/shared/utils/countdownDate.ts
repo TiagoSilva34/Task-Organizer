5a3838d5af6029f16c951ad45a768e14b257dee8
Tiago Borges <tiagoborges899@gmail.com>
2024-07-17 17:10:03 -0300

    feat: criação da função countdown

--- src/shared/utils/countdownDate.ts
+++ src/shared/utils/countdownDate.ts
@@ -1,25 +1,55 @@
+
 // Update the count down every 1 second
-export const countdownDate = function () {
-  // Set the date we're counting down to
+export const countdownDate = function (endDate: any) {  
+  // // Get today's date and time
+  // var now = new Date().getTime();
 
-  var date = new Date("Jul 7, 2024 18:18:25").getTime();
+  let day = endDate.slice(0, 2)
+  let month = endDate.slice(3, 5)
+  let year = endDate.slice(6, 10)
+  const formattedate = new Date(`${month}/${day}/${year}`).getTime()
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  // // Find the distance between now and the count down date
+  // var distance = formattedate - now;
 
-  // Find the distance between now and the count down date
-  var distance = date - now;
+  // // Time calculations for days, hours, minutes and seconds
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // // Output the result in an element with id="demo"
+  // let counter = hours + "h : " + minutes + "m : " + seconds + "s"; 
 
-  hours = hours < 10 ? hours : hours
+  // return counter
 
-  // Output the result in an element with id="demo"
-  let counter = days + " days " + hours + "h : " + minutes + "m : " + seconds + "s"; 
+  const firstDate: Date = new Date();
+ 
+// Current Date
+const secondDate: Date = new Date(formattedate);
+ 
+// Time Difference in Milliseconds
+const milliDiff: number = Math.abs(firstDate.getTime()
+    - secondDate.getTime());
+ 
+// Converting time into hh:mm:ss format
+ 
+// Total number of seconds in the difference
+const totalSeconds = Math.floor(milliDiff / 1000);
+ 
+// Total number of minutes in the difference
+const totalMinutes = Math.floor(totalSeconds / 60);
+ 
+// Total number of hours in the difference
+const totalHours = Math.floor(totalMinutes / 60);
+ 
+// Getting the number of seconds left in one minute
+const remSeconds = totalSeconds % 60;
+ 
+// Getting the number of minutes left in one hour
+const remMinutes = Number(totalMinutes) % 60;
+ 
+ let teste = `${totalHours}:${remMinutes}:${remSeconds}`;
 
-  return counter;
+ return teste
+
 };
