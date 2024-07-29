9d861732b45e180de6d8998cc1a0894ecfb7d391
Tiago Borges <tiagoborges899@gmail.com>
2024-07-29 18:18:02 -0300

    fix: corigido onSubmit para opcional

--- src/shared/components/Form/interface.ts
+++ src/shared/components/Form/interface.ts
@@ -1,4 +1,4 @@
 export interface IFormProps extends React.FormHTMLAttributes<HTMLFormElement> {
     children: React.ReactNode
-    onSubmit: React.FormEventHandler<HTMLFormElement>
+    onSubmit?: React.FormEventHandler<HTMLFormElement>
 }
