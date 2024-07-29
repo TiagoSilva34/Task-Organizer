a108332fd6ef5d7b6a03a3467b74393e15c2e9d7
Tiago Borges <tiagoborges899@gmail.com>
2024-07-29 18:18:40 -0300

    feat: adicionado propriedades setCheckedTodo e removeAnimate

--- src/pages/Task/TaskItem/interface.ts
+++ src/pages/Task/TaskItem/interface.ts
@@ -2,6 +2,8 @@ import { ITODO } from "../../../shared/models/ITodo";
 
 export interface ITodoItem {
     todo: ITODO
+    setCheckedTodo: any
+    removeAnimate: boolean
     onClickRemoveTodo: (id: string) => void
     onClickUpdateTodo: (todo: any) => void
 }
