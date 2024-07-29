fa4a5c6de22b60defd0a28910f6cf1dab073cea0
Tiago Borges <tiagoborges899@gmail.com>
2024-07-29 18:19:42 -0300

    feat: adicionado AuthProvider a aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from './App.tsx'
 import './styles/globalStyles.scss'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { AuthProvider } from './shared/contexts/auth.tsx';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+
+      </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
